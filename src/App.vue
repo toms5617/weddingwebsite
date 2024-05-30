b4539f239922cb03e15469263125eef9549dfc96
unknown <Toms Donis>
2024-05-30 19:25:57 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Alegreya:wght@400;700&display=swap');
+
+#app {
+  display: block;
+  font-family: 'Alegreya', serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #5a86cc;
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,283 @@
+<template>
+  <div class="container">
+    <img :src="flowers" alt="flowers" class="flowers-image" />
+
+    <h1>Toms & Annemarie</h1>
+    <h3
+      style="font-style: italic;
+      }"
+    >
+      are getting married
+    </h3>
+    <h3
+      style="font-weight: 700;
+      }"
+    >
+      Friday 30. May 2025
+    </h3>
+
+    <img :src="tAengagement" alt="Toms and Annemarie engagement picture" class="picture" />
+
+    <img :src="divider" alt="divider" class="divider-image" />
+
+    <h3>RSVP</h3>
+
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSfgXe1PbaDtstgmLXP4esXhHPIaemvnmWoRI_-twL_UW5f5_g/viewform?embedded=true"
+      width="100%"
+      height="450px"
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+      >Loading…</iframe
+    >
+    <h2>Itinerary</h2>
+    <div>
+      <h4>Option to arrive the night before the wedding</h4>
+      <p>Thursday 29. May 2025</p>
+      <p class="left-alligned">
+        (payment 1100 dkk/ 150 euro per person which includes dinner, drinks, events, overnight stay
+        at the manor, breakfast morning 30. May)
+      </p>
+      <p class="left-alligned">
+        What: Latvian bbq, drinks, sauna, workshops, casually settling in at the manor and meeting
+        the other guests
+      </p>
+      <p>Start: 16:00</p>
+      <p>Where: <a href="https://g.co/kgs/VHYuoJp" target="_blank">Abgunste manor</a></p>
+    </div>
+
+    <img :src="divider" alt="divider" class="divider-image" />
+
+    <div>
+      <h4>Friday 30. May 2025</h4>
+      <p>(no payment)</p>
+      <p>What: wedding ceremony, flowers, dinner and party</p>
+      <p>Start: 15:00 (wedding ceremony)</p>
+      <p>Please be at the manor 1-2 hours before, if you arrive on the day.</p>
+      <p>Where: <a href="https://g.co/kgs/VHYuoJp" target="_blank">Abgunste manor</a></p>
+    </div>
+
+    <img :src="divider" alt="divider" class="divider-image" />
+
+    <h2>Wish list</h2>
+    <p class="left-alligned">
+      If you are considering giving us a gift, we would very much appreciate money for our honeymoon
+      to South Korea.
+    </p>
+
+    <img :src="seoulPoster" alt="seoul poster" class="seoul-poster-image" />
+
+    <p>Mobile pay / revolut box?</p>
+
+    <h2>Why Abgunste Manor?</h2>
+
+    <img :src="abgunsteManor" alt="abgunste manor" class="picture" />
+
+    <h2>Local travel guide</h2>
+
+    <h3>Latvia Travel Guide</h3>
+
+    <img :src="divider" alt="divider" class="divider-image" />
+
+    <img :src="riga" alt="riga" class="picture" />
+
+    <h2>Riga</h2>
+    <div>
+      <h5>To do:</h5>
+      <ul>
+        <li>Walking tour</li>
+        <li>
+          Latvian ethnographical museum -
+          <a href="https://goo.gl/maps/1HugV6rpV3s74DH36" target="_blank">Link</a>
+        </li>
+        <li>The Corner house</li>
+        <li>Museum of the Occupation of Latvia</li>
+      </ul>
+
+      <img :src="divider" alt="divider" class="divider-image" />
+
+      <h2>To see:</h2>
+
+      <img :src="freedomMonument" alt="Freedom Monument" class="picture" />
+
+      <ul>
+        <li>Freedom monument</li>
+        <li>
+          Art nouveau/ jugendstil architecture -
+          <a
+            href="https://www.google.com/maps/place/Art+Nouveau+building,+Alberta+iela+13,+Centra+rajons,+R%C4%ABga,+LV-1010,+Latvia/@56.9601208,24.1052749,15z/data=!4m2!3m1!1s0x46eecfc5ae488137:0x55936b70a23adcf5"
+            target="_blank"
+            >Link</a
+          >
+        </li>
+        <li>St. Peter's Church Rīgas</li>
+      </ul>
+
+      <img :src="divider" alt="divider" class="divider-image" />
+
+      <h2>To eat:</h2>
+
+      <img :src="piragiVintage" alt="Piragi" class="picture" />
+
+      <ul>
+        <li>Piragi (small pies with various fillings)</li>
+        <li>Pelmeni (a type of tortellini with filling, served with sour cream)</li>
+        <li>Cold beet soup</li>
+        <li>Eclairs (popular in Latvia)</li>
+        <li>Riga black balsams (Riga's pride, try the blackcurrant version)</li>
+        <li>Laima chocolate (Latvia’s national chocolate factory)</li>
+        <li>
+          Karums -
+          <a
+            href="https://www.google.com/search?q=karums&rlz=1CDGOYI_enDK905DK906&hl=en-GB&prmd=imsxvn&sxsrf=AOaemvLDBDCF1bAPL2HDd4E4mtLhaELv5w:1633287580858&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjYgsqw9q7zAhVmx4sKHWLED6cQ_AUoAXoECAIQAQ&biw=414&bih=720&dpr=2#imgrc=2KAdGqNuw9GkBM"
+            target="_blank"
+            >Link</a
+          >
+        </li>
+      </ul>
+
+      <img :src="divider" alt="divider" class="divider-image" />
+
+      <h2>Daytrip:</h2>
+
+      <img :src="jurmala" alt="jurmala" class="picture" />
+
+      <ul>
+        <li>
+          Jūrmala (Latvian riviera with beautiful wooden houses and 30 km long beach/forest, go to
+          the "Majori" area by train)
+        </li>
+        <li>
+          Kemeri national park -
+          <a href="https://www.latvia.travel/en/sight/great-kemeri-bog-boardwalk" target="_blank"
+            >Link</a
+          >
+        </li>
+      </ul>
+
+      <img :src="divider" alt="divider" class="divider-image" />
+
+      <h2>Restaurant/Café:</h2>
+      <ul>
+        <li>Cafe Leningrad (café/bar/music venue with lots of Soviet memorabilia)</li>
+        <li>
+          Lido (Latvian chain in typical Soviet "canteen/buffet" style, traditional Latvian food at
+          good prices) - <a href="https://goo.gl/maps/N64xH1YVnYnQCi1B9" target="_blank">Link</a>
+        </li>
+        <li>
+          Riga central market (normal market and new food court in one of the halls, beware of
+          pickpockets)
+        </li>
+        <li>
+          Folkklubs (bar/music venue) -
+          <a href="https://goo.gl/maps/TLdN1ndZk9sDzQdN7" target="_blank">Link</a>
+        </li>
+      </ul>
+    </div>
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2203.4023826666635!2d23.486218713275814!3d56.4780559357859!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46ef4773cd6c7b7b%3A0xd90579d33c324f1f!2sAbgunstes%20mansion%20%2F%20manor%20Abgunste!5e0!3m2!1sen!2sdk!4v1716665366707!5m2!1sen!2sdk"
+      width="100%"
+      height="450"
+      style="border: 0"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+import divider from '@/assets/divider.png'
+import flowers from '@/assets/flowers.png'
+import seoulPoster from '@/assets/seoul-poster.png'
+import tAengagement from '@/assets/toms-annemarie-engagement.png'
+import abgunsteManor from '@/assets/abgunste-manor.png'
+import freedomMonument from '@/assets/freedom-monument.png'
+import piragiVintage from '@/assets/Piragi-vintage.png'
+import jurmala from '@/assets/jurmala.jpg'
+import riga from '@/assets/riga.png'
+
+export default {
+  name: 'FrontPage',
+  data() {
+    return {
+      divider,
+      flowers,
+      seoulPoster,
+      tAengagement,
+      abgunsteManor,
+      freedomMonument,
+      piragiVintage,
+      jurmala,
+      riga
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+}
+h1 {
+  font-size: 36px;
+}
+h2 {
+  font-size: 28px;
+}
+h3 {
+  font-size: 24px;
+  font-weight: 400;
+}
+h4 {
+  font-size: 20px;
+}
+h5 {
+  font-size: 18px;
+}
+h1,
+h2,
+h4,
+h5 {
+  color: #5a86cc;
+  font-weight: 700;
+}
+p {
+  color: #5a86cc;
+  font-weight: 400;
+  font-size: 16px;
+}
+.left-alligned {
+  text-align: left;
+}
+ul {
+  list-style-type: none;
+  padding-left: 0;
+}
+li {
+  margin-bottom: 5px;
+}
+a {
+  color: #1e88e5;
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
+.divider-image {
+  margin: 20px 0;
+  width: 60px;
+}
+.flowers-image {
+  margin: 20px 0;
+  width: 160px;
+}
+.picture {
+  margin: 20px 0;
+  width: 100%;
+}
+.seoul-poster-image {
+  margin: 20px 0;
+  width: 300px;
+}
+</style>
